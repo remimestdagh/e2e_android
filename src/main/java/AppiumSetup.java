01bf4c719144e3f1aecb2d61e7ac7cac5c2107e7
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-12-19 13:44:05 +0100

    adadd

--- src/main/java/AppiumSetup.java
+++ src/main/java/AppiumSetup.java
@@ -1,5 +1,7 @@
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,7 +14,7 @@ public class AppiumSetup {
     private static AndroidDriver driver;
 
     @Before
-    public void init() throws MalformedURLException {
+    public void init() throws MalformedURLException, InterruptedException {
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("deviceName", "emulator-5554");
         cap.setCapability("platformName", "android");
@@ -22,10 +24,8 @@ public class AppiumSetup {
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AndroidDriver(url, cap);
         System.out.println("App started");
-    }
 
-    @Test
-    public void testDagboek() throws InterruptedException {
+        Thread.sleep(2000);
         MobileElement el1 = (MobileElement) driver.findElementById("com.example.faith:id/editTextTextEmailAddress");
         el1.click();
         el1.sendKeys("jef.seys.y0431@student.hogent.be");
@@ -36,6 +36,12 @@ public class AppiumSetup {
         driver.navigate().back();
         MobileElement el3 = (MobileElement) driver.findElementById("com.example.faith:id/button_login");
         el3.click();
+        System.out.println("logged in");
+    }
+
+    @Test
+    public void testDagboek() throws InterruptedException {
+
         Thread.sleep(2000);
         MobileElement el4 = (MobileElement) driver.findElementById("com.example.faith:id/image_bib");
         el4.click();
@@ -61,6 +67,53 @@ public class AppiumSetup {
         MobileElement el11 = (MobileElement) driver.findElementById("com.example.faith:id/txtBeschrijving");
         Assert.assertEquals(el10.getAttribute("text"), "Dagboektest");
         Assert.assertEquals(el11.getAttribute("text"), "Dagboektest");
+    }
+
+    @Test
+    public void testInfoBalie() throws InterruptedException {
+        Thread.sleep(2000);
+        MobileElement el4 = (MobileElement) driver.findElementById("com.example.faith:id/image_bali");
+        el4.click();
+        Thread.sleep(2000);
+        MobileElement el5 = (MobileElement) driver.findElementByAccessibilityId("Hulpbron toevoegen");
+        el5.click();
+        Thread.sleep(2000);
+        MobileElement el6 = (MobileElement) driver.findElementById("com.example.faith:id/textInputTitel");
+        el6.click();
+        el6.sendKeys("testbron");
+        MobileElement el7 = (MobileElement) driver.findElementById("com.example.faith:id/textInputBeschrijving");
+        el7.click();
+        el7.sendKeys("testbronbeschrijving");
+        MobileElement el8 = (MobileElement) driver.findElementById("com.example.faith:id/textInputEmailadres");
+        el8.click();
+        el8.sendKeys("test@test.be");
+        driver.navigate().back();
+        MobileElement el9 = (MobileElement) driver.findElementById("com.example.faith:id/textInputTelefoonnummer");
+        el9.click();
+        el9.sendKeys("050211364");
+        driver.navigate().back();
+        MobileElement el10 = (MobileElement) driver.findElementById("com.example.faith:id/textInputUrl");
+        el10.click();
+        el10.sendKeys("www.epic.be");
+        driver.navigate().back();
+        MobileElement el11 = (MobileElement) driver.findElementById("com.example.faith:id/textInputChatUrl");
+        el11.click();
+        el11.sendKeys("www.epic.be/test");
+        driver.navigate().back();
+        MobileElement el12 = (MobileElement) driver.findElementById("com.example.faith:id/btnSaveHulpbron");
+        el12.click();
+        Thread.sleep(2000);
+        new TouchAction(driver).press(PointOption.point(550, 200)).waitAction().moveTo(PointOption.point(550, 550)).release().perform();
+        MobileElement el13 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/androidx.cardview.widget.CardView[1]/android.widget.LinearLayout");
+        el13.click();
+        Thread.sleep(2000);
+
+
+
+        MobileElement el14 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/androidx.cardview.widget.CardView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.TextView");
+        MobileElement el15 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/androidx.cardview.widget.CardView/android.widget.LinearLayout/android.widget.TextView");
+        Assert.assertEquals(el14.getAttribute("text"), "testbron");
+        Assert.assertEquals(el15.getAttribute("text"), "testbronbeschrijving");
 
 
     }
