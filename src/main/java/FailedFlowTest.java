b36962a3e4a45bcd0f189f38a59f55ca727b8534
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-12-19 15:48:29 +0100

    added failing logintest/ infobalie/ chat/ dagboek/ cinema

--- src/main/java/FailedFlowTest.java
+++ src/main/java/FailedFlowTest.java
@@ -1,6 +1,10 @@
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -33,5 +37,69 @@ public class FailedFlowTest {
         MobileElement el3 = (MobileElement) driver.findElementById("com.example.faith:id/button_login");
         el3.click();
         System.out.println("logged in");
+
+    }
+
+    @Test
+    public void testDagboek() throws InterruptedException {
+
+        Thread.sleep(2000);
+        MobileElement el4 = (MobileElement) driver.findElementById("com.example.faith:id/image_bib");
+        el4.click();
+        Thread.sleep(2000);
+        MobileElement el5 = (MobileElement) driver.findElementByAccessibilityId("add_dagboek");
+        el5.click();
+        Thread.sleep(2000);
+        MobileElement el6 = (MobileElement) driver.findElementById("com.example.faith:id/textInputTitelDagboek");
+        el6.click();
+        el6.sendKeys("");
+        driver.navigate().back();
+        MobileElement el7 = (MobileElement) driver.findElementById("com.example.faith:id/textInputDescription");
+        el7.click();
+        el7.sendKeys("Dagboektest");
+        driver.navigate().back();
+        MobileElement el8 = (MobileElement) driver.findElementById("com.example.faith:id/btSaveDagboek");
+        el8.click();
+        Thread.sleep(2000);
+        Assert.assertTrue(Boolean.parseBoolean(el8.getAttribute("displayed")));
+
+    }
+
+    @Test
+    public void testInfoBalie() throws InterruptedException {
+        Thread.sleep(2000);
+        MobileElement el4 = (MobileElement) driver.findElementById("com.example.faith:id/image_bali");
+        el4.click();
+        Thread.sleep(2000);
+        MobileElement el5 = (MobileElement) driver.findElementByAccessibilityId("Hulpbron toevoegen");
+        el5.click();
+        Thread.sleep(2000);
+        MobileElement el6 = (MobileElement) driver.findElementById("com.example.faith:id/textInputTitel");
+        el6.click();
+        el6.sendKeys("");
+        MobileElement el7 = (MobileElement) driver.findElementById("com.example.faith:id/textInputBeschrijving");
+        el7.click();
+        el7.sendKeys("");
+        MobileElement el8 = (MobileElement) driver.findElementById("com.example.faith:id/textInputEmailadres");
+        el8.click();
+        el8.sendKeys("test@test.be");
+        driver.navigate().back();
+        MobileElement el9 = (MobileElement) driver.findElementById("com.example.faith:id/textInputTelefoonnummer");
+        el9.click();
+        el9.sendKeys("050211364");
+        driver.navigate().back();
+        MobileElement el10 = (MobileElement) driver.findElementById("com.example.faith:id/textInputUrl");
+        el10.click();
+        el10.sendKeys("www.epic.be");
+        driver.navigate().back();
+        MobileElement el11 = (MobileElement) driver.findElementById("com.example.faith:id/textInputChatUrl");
+        el11.click();
+        el11.sendKeys("www.epic.be/test");
+        driver.navigate().back();
+        MobileElement el12 = (MobileElement) driver.findElementById("com.example.faith:id/btnSaveHulpbron");
+        el12.click();
+        Thread.sleep(2000);
+        Assert.assertTrue(Boolean.parseBoolean(el12.getAttribute("displayed")));
+
     }
 }
